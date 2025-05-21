3ee37ddb0b4381663a44113755e2d9276cd869a4
Joe Yang <joe.yang@alaskaair.com>
2025-05-20 17:58:45 -0700

    refactor: switch to Embla (WIP)

--- src/auro-slideshow.js
+++ src/auro-slideshow.js
@@ -3,16 +3,11 @@
 
 // ---------------------------------------------------------------------
 
-import { LitElement } from "lit";
+import { LitElement, nothing } from "lit";
 import { html } from "lit/static-html.js";
 
-// import EmblaCarousel from "embla-carousel";
-// import Autoplay from "embla-carousel-autoplay";
-
-// const emblaNode = document.querySelector(".embla");
-// const options = { loop: false };
-// const plugins = [Autoplay()];
-// const emblaApi = EmblaCarousel(emblaNode, options, plugins);
+import EmblaCarousel from "embla-carousel";
+import Autoplay from "embla-carousel-autoplay";
 
 import { AuroDependencyVersioning } from "@aurodesignsystem/auro-library/scripts/runtime/dependencyTagVersioning.mjs";
 import AuroLibraryRuntimeUtils from "@aurodesignsystem/auro-library/scripts/utils/runtimeUtils.mjs";
@@ -26,9 +21,9 @@ import iconVersion from "./iconVersion.js";
 import chevronLeft from "@alaskaairux/icons/dist/icons/interface/chevron-left.mjs";
 import chevronRight from "@alaskaairux/icons/dist/icons/interface/chevron-right.mjs";
 
-import arrowDown from "@alaskaairux/icons/dist/icons/interface/arrow-down.mjs";
-// These should be replaced with pause/play icons when created by design
-import arrowUp from "@alaskaairux/icons/dist/icons/interface/arrow-up.mjs";
+// TODO: These should be replaced with pause/play icons when created by design
+import fakePlay from "@alaskaairux/icons/dist/icons/in-flight/entertainment-stroke.mjs";
+import fakePause from "@alaskaairux/icons/dist/icons/in-flight/luggage-stroke.mjs";
 
 import styleCss from "./style.scss";
 
@@ -36,19 +31,24 @@ export class AuroSlideshow extends LitElement {
   constructor() {
     super();
 
-    this.autoplay = 7000;
-    this.pagination = false;
+    this.autoplay = false;
+    this.playOnInit = false;
+    this.delay = 5000;
+
+    this.autoScroll = false;
+
     this.loop = false;
-    this.slidesPerView = "auto";
-    this.spaceBetweenSlides = 16;
-    this.variant = "slideshow";
+    this.navigation = false;
+    this.pagination = false;
+
+    this.playBtnLabel = "Play slideshow";
+
+    // private
+    this.playIcon = fakePlay.svg;
+    this.embla = null;
+    this.slides = [];
 
     const versioning = new AuroDependencyVersioning();
-
-    /**
-     * @private
-     */
-    this.isPlaying = true;
 
     /**
      * @private
@@ -82,25 +82,40 @@ export class AuroSlideshow extends LitElement {
   static get properties() {
     return {
       /**
-       * The time in milliseconds between each slide change. Defaults to undefined.
+       * If true, the slideshow will play automatically.
        */
       autoplay: {
+        type: Boolean,
+        reflect: true,
+      },
+      /**
+       * If true, the slideshow will start playing automatically when initialized.
+       */
+      playOnInit: {
+        type: Boolean,
+        reflect: true,
+      },
+      /**
+       * Slide duration in milliseconds.
+       */
+      delay: {
         type: Number,
         reflect: true,
       },
-
-      isPlaying: {
-        type: Boolean,
-      },
-
       /**
-       * If true, the slideshow will loop back to the first slide after reaching the last slide. Defaults to false.
+       * If true, the slideshow will loop back to the first slide after reaching the last slide.
        */
       loop: {
         type: Boolean,
         reflect: true,
       },
-
+      /**
+       * If true, the slideshow will display navigation arrows for previous and next slides.
+       */
+      navigation: {
+        type: Boolean,
+        reflect: true,
+      },
       /**
        * If true, the slideshow will display pagination bullets for each slide.
        */
@@ -108,215 +123,117 @@ export class AuroSlideshow extends LitElement {
         type: Boolean,
         reflect: true,
       },
-
-      /**
-       * The number of slides per view. Defaults to 1.
-       */
-      slidesPerView: {
-        type: String,
-        reflect: true,
-      },
-
-      /**
-       * The pixel distance between slides when multiple slides are in view.
-       */
-      spaceBetweenSlides: {
-        type: Number,
-      },
-
-      /**
-       * 'slideshow': pagination indicators will be showing underneat with auto-play the progressbar
-       * `slider`: prev/next button will show on hover and there will be no pagination indicator
-       * @default 'slidershow'
-       */
-      variant: {
-        // 'slideshow', 'slider'
-        type: String,
-        reflect: true,
-      },
     };
+  }
+
+  get _playBtn() {
+    return this.shadowRoot.querySelector(".play-pause");
+  }
+
+  initializeEmbla() {
+    const emblaNode = this.shadowRoot.querySelector(".embla");
+    const options = { loop: this.loop };
+    let plugins = null;
+
+    const autoplayOptions = {
+      playOnInit: false,
+      delay: this.delay,
+    };
+
+    const autoscrollOptions = {};
+
+    if (this.autoplay) {
+      plugins = [Autoplay(autoplayOptions)];
+    }
+    if (this.autoScroll) {
+      plugins = [Autoscroll(autoscrollOptions)];
+    }
+
+    // Attach slides to the Embla instance
+    const emblaContainer = this.shadowRoot.querySelector(".embla__container");
+    emblaContainer.replaceChildren(...this.slides);
+
+    this.embla = EmblaCarousel(emblaNode, options, plugins);
+
+    this.addPlayBtnListeners(this.embla, this._playBtn);
+
+    if (this.playOnInit) {
+      this.embla.plugins().autoplay.play();
+    }
   }
 
   firstUpdated() {
-    console.log("first updated");
-    this.handleHeaderSlotContent();
-
-    const slot = this.shadowRoot.querySelector(
-      'slot:not([name="header"]):not([name="subheader"])',
-    );
-
-    // Listen for the slotchange event to handle updates to slot content
-    this.slotChangeListener = () => {
-      this.initializeSwiper();
-    };
-
-    slot.addEventListener("slotchange", this.slotChangeListener);
+    this.updateSlides();
+    this.initializeEmbla();
+    console.log(this.embla.plugins().autoplay.isPlaying());
+    console.log("first updated", this.slides);
   }
 
-  handleHeaderSlotContent() {
-    const container = this.shadowRoot.querySelector(".container");
-    const headerSlot = this.shadowRoot.querySelector('slot[name="header"]');
-    const subheaderSlot = this.shadowRoot.querySelector(
-      'slot[name="subheader"]',
-    );
-
-    const hasHeaderContent =
-      headerSlot && headerSlot.assignedNodes().length > 0;
-    const hasSubheaderContent =
-      subheaderSlot && subheaderSlot.assignedNodes().length > 0;
-
-    if (hasHeaderContent && hasSubheaderContent) {
-      container.classList.add("has-both-headers");
-    } else if (hasHeaderContent) {
-      container.classList.add("has-header-only");
-    } else if (hasSubheaderContent) {
-      container.classList.add("has-subheader-only");
-    } else {
-      container.classList.add("has-no-headers");
-    }
+  handleSlotChange() {
+    // this.updateSlides();
+    // this.initializeEmbla();
   }
 
-  initializeSwiper() {
-    const swiperElement = this.shadowRoot.querySelector(".swiper");
-    const swiperWrapper = this.shadowRoot.querySelector(".swiper-wrapper");
-    const prevButton = this.shadowRoot.querySelector(".scroll-prev");
-    const nextButton = this.shadowRoot.querySelector(".scroll-next");
-    const playPauseButton = this.shadowRoot.querySelector(".play-pause");
-    const paginationEl = this.shadowRoot.querySelector(".swiper-pagination");
-    const slot = this.shadowRoot.querySelector(
-      'slot:not([name="header"]):not([name="subheader"])',
-    );
-
-    if (!slot) {
-      return;
-    }
-
-    // Get the assigned slides from the slot
-    const assignedSlides = slot.assignedElements();
-
-    // Avoid reinitializing Swiper if there are no slides or if the Swiper is already initialized
-    if (assignedSlides.length === 0) {
-      console.warn("No slides found inside slot.");
-      return; // Avoid initializing if no slides are present
-    }
-
-    assignedSlides.forEach((slide) => {
-      slide.classList.add("swiper-slide");
-      slide.part = "slide";
+  updateSlides() {
+    const slot = this.shadowRoot.querySelector("slot:not([name])");
+    slot.assignedElements().forEach((element) => {
+      element.classList.add("embla__slide");
     });
+    this.slides = slot.assignedElements();
+  }
 
-    // Detach the slotchange listener to prevent it from firing while manipulating the slot content
-    slot.removeEventListener("slotchange", this.slotChangeListener);
+  addPlayBtnListeners = (emblaApi, playBtn) => {
+    const togglePlayBtnState = (emblaApi) => {
+      const autoplay = emblaApi?.plugins()?.autoplay;
+      if (!autoplay) return;
 
-    // Ensure we don't clear the swiper-wrapper content if the slides are updated dynamically
-    if (!this.swiper) {
-      // If no swiper instance exists, clear the swiper-wrapper and add the new slides
-      swiperWrapper.innerHTML = ""; // Clear the wrapper, only if needed
-      assignedSlides.forEach((slide) => {
-        swiperWrapper.appendChild(slide);
-      });
-    }
-
-    const swiperConfig = {
-      modules: [Navigation, Pagination, Autoplay],
-      loop: this.loop,
-      slidesPerView: this.slidesPerView,
-      spaceBetween: this.spaceBetweenSlides,
-      centeredSlides: false,
-      autoplay: this.autoplay
-        ? {
-            delay: this.autoplay,
-            disableOnInteraction: false,
-            pauseOnMouseEnter: true,
-          }
-        : false,
-      pagination: {
-        el: paginationEl,
-        clickable: true,
-        renderBullet: (_, className) => {
-          return `
-            <span class="${className}"  tabindex="0">
-              <div class="pagination-swiper-up__progress-bar-container">
-                <div class="pagination-swiper-up__progress"></div>
-              </div>
-            </span>
-          `;
-        },
-      },
+      // Not sure why these are reversed. Embla is weird.
+      this.playBtnLabel = autoplay.isPlaying()
+        ? "Play Slideshow"
+        : "Pause Slideshow";
+      this.playIcon = autoplay.isPlaying() ? fakePlay.svg : fakePause.svg;
+      this.requestUpdate();
     };
 
-    if (this.variant === "slider") {
-      swiperConfig.navigation = {
-        nextEl: nextButton,
-        prevEl: prevButton,
-      };
-    }
+    const onPlayBtnClick = () => {
+      const autoplay = emblaApi?.plugins()?.autoplay;
+      if (!autoplay) return;
 
-    if (this.autoplay) {
-      playPauseButton.addEventListener("click", () => {
-        if (this.swiper.autoplay.running) {
-          this.swiper.autoplay.stop();
-          this.isPlaying = false;
-          playPauseButton.setAttribute("aria-label", "play");
-        } else {
-          this.swiper.autoplay.start();
-          this.isPlaying = true;
-          playPauseButton.setAttribute("aria-label", "pause");
-        }
-      });
-    }
+      const playOrStop = autoplay.isPlaying() ? autoplay.stop : autoplay.play;
+      playOrStop();
+      console.log("Playing?", autoplay.isPlaying());
+    };
 
-    // If the Swiper instance exists, just update it with the new slides
-    if (this.swiper) {
-      this.swiper.update();
-    } else {
-      this.swiper = new Swiper(swiperElement, swiperConfig);
-    }
+    playBtn.addEventListener("click", onPlayBtnClick);
+    emblaApi
+      .on("autoplay:play", togglePlayBtnState)
+      .on("autoplay:stop", togglePlayBtnState)
+      .on("reInit", togglePlayBtnState);
 
-    const allBullets = this.shadowRoot.querySelectorAll(
-      ".swiper-pagination-bullet",
-    );
+    return () => {
+      playBtn.removeEventListener("click", onPlayBtnClick);
+      emblaApi
+        .off("autoplay:play", togglePlayBtnState)
+        .off("autoplay:stop", togglePlayBtnState)
+        .off("reInit", togglePlayBtnState);
+    };
+  };
 
-    allBullets.forEach((bullet) => {
-      bullet.addEventListener("keydown", (event) => {
-        if (event.key === "Enter" || event.key === " ") {
-          event.preventDefault(); // Prevent scrolling if space is used
-          bullet.click(); // Simulate a click event
-        }
-      });
-    });
+  /**
+   * Toggles the play/pause state of the slideshow.
+   * @returns {void}
+   */
+  togglePlay() {
+    console.log("play clicked");
+    const autoplay = this.embla?.plugins()?.autoplay;
 
-    // TODO: Add logic to the progress bar for pausing and playing the slideshow
-    this.swiper.on("slideChange", () => {
-      const activeBullet = this.shadowRoot.querySelector(
-        ".swiper-pagination-bullet-active",
-      );
+    autoplay.isPlaying() ? autoplay.stop : autoplay.play;
 
-      // Reset all progress bars
-      allBullets.forEach((bullet) => {
-        const progressBar = bullet.querySelector(
-          ".pagination-swiper-up__progress",
-        );
-        if (progressBar) {
-          progressBar.style.transition = "none"; // Disable transition for reset
-          progressBar.style.width = "0%"; // Reset width for all bullets
-        }
-      });
+    // this.togglePlayBtnState()
 
-      // Animate the progress bar of the active bullet
-      if (activeBullet) {
-        const progressBar = activeBullet.querySelector(
-          ".pagination-swiper-up__progress",
-        );
-        if (progressBar) {
-          progressBar.style.transition = `width ${swiperConfig.autoplay.delay}ms linear`;
-          progressBar.style.width = "100%"; // Fill progress bar to 100%
-        }
-      }
-    });
-
-    // Reattach the slotchange event listener after the swiper is initialized
-    slot.addEventListener("slotchange", this.slotChangeListener);
+    // this.isPlaying = !this.isPlaying;
+    // rerender elements
+    // this.requestUpdate();
   }
 
   /**
@@ -337,44 +254,70 @@ export class AuroSlideshow extends LitElement {
     return iconHtml;
   }
 
+  /**
+   * Generates the HTML for the navigation controls (previous and next buttons).
+   * @private
+   * @returns {Element} The HTML element containing the navigation controls.
+   */
+  renderNavigationControls() {
+    return html`
+      <${this.buttonTag} 
+        arialabel="Previous slide" 
+        class="scroll-prev"
+        iconOnly rounded 
+        variant="secondary" 
+        @click=${() => this.embla.scrollPrev()}>
+        ${this.generateIconHtml(chevronLeft.svg)}
+      </${this.buttonTag}>
+      <${this.buttonTag} 
+        arialabel="Next slide" 
+        class="scroll-next"
+        iconOnly rounded 
+        variant="secondary" 
+        @click=${() => this.embla.scrollNext()}>
+        ${this.generateIconHtml(chevronRight.svg)}
+      </${this.buttonTag}>`;
+  }
+
+  renderPlayButton() {
+    return html`
+    <${this.buttonTag} 
+      arialabel="${this.playBtnLabel}"
+      class="play-pause"
+      iconOnly 
+      rounded 
+      >
+        ${this.generateIconHtml(this.playIcon)}
+    </${this.buttonTag}>
+    `;
+  }
+
+  /**
+   * Generates the HTML for the pagination controls.
+   * @private
+   * @returns {Element} The HTML element containing the pagination controls.
+   */
+  renderPaginationDots() {
+    return html`
+      <div class="embla__dots">
+      </div>
+    `;
+  }
+
   render() {
     return html`
       <div class="container">
-        <slot name="header"></slot>
-        <slot name="subheader"></slot>
         <div class="slideshow-wrapper">
-          <div class="swiper">
-            <div class="swiper-wrapper">
-              <slot></slot>
+          ${this.navigation ? this.renderNavigationControls() : nothing}
+          <div class="embla">
+            <div class="embla__container">
+              <slot @slotchange=${this.handleSlotChange}></slot>
             </div>
           </div>
-
-            <${this.buttonTag}
-            class="chevron-left"
-            arialabel="Previous Item"
-            rounded
-            iconOnly
-            @click=${() => this.swiper.slidePrev()}>
-              ${this.generateIconHtml(chevronLeft.svg)}
-            </${this.buttonTag}>
-            <${this.buttonTag}
-            class="chevron-right"
-            arialabel="Next Item"
-            rounded
-            iconOnly
-            @click=${() => this.swiper.slideNext()}>
-              ${this.generateIconHtml(chevronRight.svg)}
-            </${this.buttonTag}>
-          
         </div>
-  
         <div class="pagination-container">
-          <${this.buttonTag} arialabel="play-pause" iconOnly rounded class="play-pause">
-            ${this.generateIconHtml(arrowUp.svg, !this.isPlaying)}
-            ${this.generateIconHtml(arrowDown.svg, this.isPlaying)}
-            <span class="util_displayHiddenVisually">Play/Pause</span>
-          </${this.buttonTag}>
-          <div class="swiper-pagination"></div>
+          ${this.autoplay ? this.renderPlayButton() : nothing}
+          ${this.pagination ? this.renderPaginationDots() : nothing}
         </div>
       </div>
     `;
